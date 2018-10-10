1be95143d7175d9f260cbc2513343f0640606032
jhuesser <github@jhuesser.ch>
2018-10-10 16:45:05 +0200

    remove again

--- functions/telegram.php
+++ functions/telegram.php
@@ -2,7 +2,7 @@
 
 function sendMessage($message){
 	$chatID  					=	$message['chatID'];
-	$text 	 					=	utf8_encode($message['text']);
+	$text 	 					=	$message['text'];
 	$parse_mode  				=	$message['parse_mode'];
 	$disable_web_page_preview 	=	$message['disable_web_page_preview'];
 	$disable_notification 	 	=	$message['disable_notification'];
