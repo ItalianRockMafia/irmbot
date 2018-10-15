dbfdc45c2854d1c94e68e6b0a452421c97bc5e68
jhuesser <github@jhuesser.ch>
2018-10-15 13:53:54 +0200

    ffixed spacing

--- bot.php
+++ bot.php
@@ -18,6 +18,7 @@ $update = getUpdates($latest);
 if($latest == $update['update_id']){
 	exit("No new updates.");
 }
+
 file_put_contents("latest.txt", $update['update_id']);
 
 
@@ -27,8 +28,8 @@ $msg = $message['text'];
 if($msg[0] == "/"){
 	switch (true){
 		case stripos($msg, "/events") !== false:
-		$response = "<b>All events:</b>" . chr(10) . chr(10);	
-		$events = json_decode(file_get_contents(IRM_API_ROOT . "events?transform=1&order=startdate,asc"), true);
+			$response = "<b>All events:</b>" . chr(10) . chr(10);	
+			$events = json_decode(file_get_contents(IRM_API_ROOT . "events?transform=1&order=startdate,asc"), true);
 			foreach($events['events'] as $event){
 				$date = new DateTime();
 
@@ -43,16 +44,16 @@ if($msg[0] == "/"){
 				}
 				$msg2send['parse_mode'] = "HTML";
 			}
-			break;
+		break;
+		
 		case stripos($msg, "/event") !== false:
-				$res = explode(' ', $msg);
-				$term = $res[1];
-				$eventResults = json_decode(file_get_contents(IRM_API_ROOT . "events?transform=1&order=startdate,asc&filter=event_title,cs," . $term), true);
-				$response = "<b>Found events:</b>" . chr(10) . chr(10);
+			$res = explode(' ', $msg);
+			$term = $res[1];
+			$eventResults = json_decode(file_get_contents(IRM_API_ROOT . "events?transform=1&order=startdate,asc&filter=event_title,cs," . $term), true);
+			$response = "<b>Found events:</b>" . chr(10) . chr(10);
 
 			foreach($eventResults['events'] as $event){
-
-				
+			
 				$date = new DateTime();
 				$eventURL = "https://italianrockmafia.ch/meetup/event.php?event=" . $event['eventID'];
 
@@ -67,68 +68,68 @@ if($msg[0] == "/"){
 				$msg2send['parse_mode'] = "HTML";
 				}
 
-			break;
+		break;
+		
 		case stripos($msg, "/details") !== false:
-				$arr = explode(' ', $msg);
-				$eventID = $arr[1];
-				$events = json_decode(file_get_contents(IRM_API_ROOT . "eventUsers?filter=eventID,eq," . $eventID . "&transform=1"), true);
-				$eventDetails = "<b>Event details:</b>" . chr(10);
-				foreach($events['eventUsers'] as $event){
-					$startdate = new DateTime($event['startdate']);
-					$enddate = new DateTime($event['enddate']);
-					$eventDetails .= $event['event_title'] . chr(10);
-					$eventDetails .= "Start: " . $startdate->format('d.m.Y H:i') .chr(10);
-					$eventDetails .= "End:   " . $enddate->format('d.m.Y H:i') . chr(10);
-					$eventDetails .= 'URL: <a href="' . $event['url'] . '">' . $event['url'] . '</a>' . chr(10);
-					$eventDetails .= "Location: " . $event['station'] . chr(10);
-					$eventDetails .= "Details:" .chr(10) . $event['description'] . chr(10);
-					$eventDetails .= 'Creator: <a href="tg://user?id=' . $event['telegramID'] . '">' . $event['tgusername'] . '</a>'. chr(10) . chr(10);
-				}
-				$attMembers = json_decode(file_get_contents(IRM_API_ROOT . "eventAttendes?transform=1&filter=eventIDFK,eq," . $eventID), true);
-				$attMsg = "<b>Attendes:</b>" . chr(10);
-				foreach($attMembers['eventAttendes'] as $attende){
-					$attMsg .= '<a href="tg://user?id=' . $attende['telegramID'] . '">' . $attende['firstname'] . ' ' . $attende['lastname'] . '</a>' . chr(10);
-				}
-				$response = $eventDetails . $attMsg;
-				$msg2send['parse_mode'] = "HTML";
-				break;
+			$arr = explode(' ', $msg);
+			$eventID = $arr[1];
+			$events = json_decode(file_get_contents(IRM_API_ROOT . "eventUsers?filter=eventID,eq," . $eventID . "&transform=1"), true);
+			$eventDetails = "<b>Event details:</b>" . chr(10);
+			foreach($events['eventUsers'] as $event){
+				$startdate = new DateTime($event['startdate']);
+				$enddate = new DateTime($event['enddate']);
+				$eventDetails .= $event['event_title'] . chr(10);
+				$eventDetails .= "Start: " . $startdate->format('d.m.Y H:i') .chr(10);
+				$eventDetails .= "End:   " . $enddate->format('d.m.Y H:i') . chr(10);
+				$eventDetails .= 'URL: <a href="' . $event['url'] . '">' . $event['url'] . '</a>' . chr(10);
+				$eventDetails .= "Location: " . $event['station'] . chr(10);
+				$eventDetails .= "Details:" .chr(10) . $event['description'] . chr(10);
+				$eventDetails .= 'Creator: <a href="tg://user?id=' . $event['telegramID'] . '">' . $event['tgusername'] . '</a>'. chr(10) . chr(10);
+			}
+			$attMembers = json_decode(file_get_contents(IRM_API_ROOT . "eventAttendes?transform=1&filter=eventIDFK,eq," . $eventID), true);
+			$attMsg = "<b>Attendes:</b>" . chr(10);
+			foreach($attMembers['eventAttendes'] as $attende){
+				$attMsg .= '<a href="tg://user?id=' . $attende['telegramID'] . '">' . $attende['firstname'] . ' ' . $attende['lastname'] . '</a>' . chr(10);
+			}
+			$response = $eventDetails . $attMsg;
+			$msg2send['parse_mode'] = "HTML";
+		break;
 
 		case stripos($msg, "/getAlbum") !== false:
-		$info = explode(' ', $msg);
-		$album = $info[2];
-		$artist = $info[1];
-		$callurl = LAST_API_ROOT . "method=album.getinfo&api_key=" . $lastfmToken . "&album=" . $album . "&artist=" . $artist . "&format=json";
-				$last_album = json_decode(file_get_contents($callurl), true);
+			$info = explode(' ', $msg);
+			$album = $info[2];
+			$artist = $info[1];
+			$callurl = LAST_API_ROOT . "method=album.getinfo&api_key=" . $lastfmToken . "&album=" . $album . "&artist=" . $artist . "&format=json";
+			$last_album = json_decode(file_get_contents($callurl), true);
 
-				for ($i=0; $i < count($last_album['album']['image']); $i++) { 
-			
-					if($last_album['album']['image'][$i]['size'] == 'extralarge') {
-									$largeImg = "";
-									$largeImg = $last_album['album']['image'][$i]['#text']; 
-					} 
-				  }
-				  $response = "<b>Album " . $last_album['album']['name'] . " by " .  $last_album['album']['artist'] . '</b>' . chr(10) . chr(10) . "<i>Tracklist:</i>" . chr(10);
-
-				  for ($i=0; $i < count($last_album['album']['tracks']['track']); $i++){
+			for ($i=0; $i < count($last_album['album']['image']); $i++) { 
+				if($last_album['album']['image'][$i]['size'] == 'extralarge') {
+					$largeImg = "";
+					$largeImg = $last_album['album']['image'][$i]['#text']; 
+				} 
+			}
+			  $response = "<b>Album " . $last_album['album']['name'] . " by " .  $last_album['album']['artist'] . '</b>' . chr(10) . chr(10) . "<i>Tracklist:</i>" . chr(10);
+			  for ($i=0; $i < count($last_album['album']['tracks']['track']); $i++){
 					$response .= $last_album['album']['tracks']['track'][$i]['name'] . chr(10);
-				}
-				$response .= '<a href="' . $last_album['album']['url'] . '">View online</a>';
+			}
+			$response .= '<a href="' . $last_album['album']['url'] . '">View online</a>';
 
-				  $pic2send['chatID'] = $message['chat']['id'];
-				  $pic2send['photo'] = $largeImg;
-				  $result = sendPhoto($pic2send);
-				  $msg2send['parse_mode'] = "HTML";
-				  $msg2send['disable_web_page_preview'] = true;
+			$pic2send['chatID'] = $message['chat']['id'];
+			$pic2send['photo'] = $largeImg;
+			$result = sendPhoto($pic2send);
+			$msg2send['parse_mode'] = "HTML";
+			$msg2send['disable_web_page_preview'] = true;
 		break;
 		
 	
 		case stripos($msg, "/ping") !== false:
-				$response = "pong";
-				break;
+			$response = "pong";
+		break;
+		
 		case stripos($msg, "/echo") !== false:
-				$split = explode(" ", $msg);
-				$response = $split[1];
-				break;
+			$split = explode(" ", $msg);
+			$response = $split[1];
+		break;
 		
 	}
 } else{
