f77976b5e305d161ef14a62746d1fb0175b32cbe
jhuesser <github@jhuesser.ch>
2018-10-16 10:25:35 +0200

    moooar documentation

--- functions/updates.php
+++ functions/updates.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * Get new messages
+ * 
+ * calls the telegram Bot API to get all new updates.
+ * 
+ * @param string $offset The latest update the bot has received.
+ * @return json All new updates
+ * @author Jonas Huesser <j.huesser@domayntec.ch>
+ * @since 0.1
+ */
 function getUpdates($offset){
 	global $token;
 	$updates = file_get_contents("https://api.telegram.org/bot" . $token . "/getUpdates?offset" . $offset);
