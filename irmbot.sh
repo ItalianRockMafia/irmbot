93ffb92197a6b12a806a9904122a3afb6aab233a
jhuesser <github@jhuesser.ch>
2018-10-16 14:16:06 +0200

    rewrite wget

--- irmbot.sh
+++ irmbot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 token=$(<token.txt)
 offset=$(<latest.txt)
-updates="`wget -qO- https://api.telegram.org/bot{$token}/getUpdates`"
+updates="`wget --quiet \
+  --method GET \
+  --header 'cache-control: no-cache' \
+  --header 'Postman-Token: a96e850e-38d1-45b3-afd7-f99f7d2b6e26' \
+  --output-document \
+  - https://api.telegram.org/bot$token/getUpdates`"
 
 wget -q \
  	--method POST \
