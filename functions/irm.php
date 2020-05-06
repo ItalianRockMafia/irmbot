b879f807792bd745a1e0933a8065f3d06039e35f
jhuesser <github@jhuesser.ch>
2020-05-06 21:01:48 +0200

    add admin checker

--- functions/irm.php
+++ functions/irm.php
@@ -25,3 +25,27 @@ function checkBotAccess($tgID) {
 
     return $access;
 }
+
+function checkBotAdminAccess($tgID){
+    global $config;
+    $callURL = $config->api_url . "/users?transform=1&filter=telegramID,eq," . $tgID;
+    $user = json_decode(file_get_contents($callURL), true);
+
+    switch ($user['accessIDFK']) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+            $access = false;
+        break;
+        case '6':
+        case '7':
+            $access = false;
+            break;
+        default:
+            $access = false;
+        break;
+    }
+
+}
