94630e158b683606349435a931018ac5dfa2e166
jhuesser <github@jhuesser.ch>
2020-05-06 21:51:34 +0200

    send message

--- bot.php
+++ bot.php
@@ -25,8 +25,10 @@ if (stripos($message, "/event") === 0) {
         $keyboard = array();
         $keyboard['InlineKeyboardMarkup'] = array();
         $keyboard['InlineKeyboardMarkup']['text'] = "test";
+        $buttons = json_encode($keyboard);
 
-        $callURL = $tg_api . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($eventMsg) . "&reply_markup=" . $keyboard;
+        $callURL = $tg_api . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($eventMsg) . "&reply_markup=" . $buttons;
+        
 
     } else {
         $msg = "Please use this command in private.";
